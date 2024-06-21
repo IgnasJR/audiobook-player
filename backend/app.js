e6132c55ae4bccf4e528501cf20b5872361ae331
Ignas K. <ignas.kanapinskas@gmail.com>
2024-06-21 18:29:25 +0300

    added SSL for frontend and backend

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,8 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const https = require("https");
+const fs = require("fs");
 const { setupExpress } = require("./controllers/express");
 
 const setup = () => {
@@ -9,9 +11,20 @@ const setup = () => {
   app.use(express.json());
   setupExpress(app);
 
-  app.listen(3001, () => {
-    console.log(`Server listening on port 3001`);
-  });
+  const privateKey = fs.readFileSync("../ssl/key.pem", "utf8");
+  const certificate = fs.readFileSync("../ssl/cert.pem", "utf8");
+  const credentials = { key: privateKey, cert: certificate };
+
+  Promise.all([privateKey, certificate])
+    .then(() => {
+      const httpsServer = https.createServer(credentials, app);
+      httpsServer.listen(3001, () => {
+        console.log(`HTTPS Server running on port 3001`);
+      });
+    })
+    .catch((error) => {
+      console.error("Failed to read credentials:", error);
+    });
 };
 
 setup();
