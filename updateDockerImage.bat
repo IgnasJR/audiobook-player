499be45321295dff62f5d2fbc6f0d5ce502e75db
Ignas K. <ignas.kanapinskas@gmail.com>
2024-07-04 18:22:10 +0300

    modified front end docker image to serve a build instead of running it

--- updateDockerImage.bat
+++ updateDockerImage.bat
@@ -3,5 +3,5 @@ docker build -t audiobook-player .
 docker push ignasjr/player-frontend
 cd ..
 cd backend
-docker build -t audiobook-player .
+docker build -t ignasjr/player-backend .
 docker push ignasjr/player-backend 
