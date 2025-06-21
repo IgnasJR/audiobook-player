8c3ee030529308d98572446a219f5a7463b13b22
IgnasJR <ignas.kanapinskas@gmail.com>
2025-06-21 12:02:29 +0300

    renamed frontend folder

--- updateDockerImage.bat
+++
@@ -1,7 +0,0 @@
-cd audiobook-player
-docker image build -t ignasjr/player-frontend .
-docker push ignasjr/player-frontend
-cd ..
-cd backend
-docker build -t ignasjr/player-backend .
-docker push ignasjr/player-backend 
